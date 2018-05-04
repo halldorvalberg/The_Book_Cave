b4b8cfb84b8be5eb22e390c774b2d13468a73e8b
oddnyk <oddny10@ru.is>
2018-05-04 22:00:04 +0000

    Category tengt við gagnagrunn

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,8 @@ namespace The_Book_Cave.Data
        public DbSet<Book> Books {get; set;}
        public DbSet<Author> Authors {get; set;}
 
+       public DbSet<Category> Categories {get; set;}
+
        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
        {
            optionsBuilder
