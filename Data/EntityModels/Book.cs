c6913fd4276f76afadd494624740ccfbb7ffce18
Halldór Valberg <halldorvs17@ru.is>
2018-05-08 13:08:44 +0000

    push

--- Data/EntityModels/Book.cs
+++ Data/EntityModels/Book.cs
@@ -9,25 +9,17 @@ namespace The_Book_Cave.Data.EntityModels
     public int ISBN { get; set; }
     public string Publisher { get; set; }
     public DateTime PublicationYear { get; set; }
-
     public int Price { get; set; }
-  
     public double Rating { get; set; }
     public string Summary { get; set; }
     public string Review { get; set; }
     public int Pages { get; set; }
     public string Type { get; set; }
-    
     public string Language { get; set; }
     public int Quantity { get; set; }
     public string Image { get; set;}
-
     public int AuthorId {get; set;}
-
     public int CategoryId {get; set;}
-
     public int InStock {get; set;}
    }
-
-
 }
