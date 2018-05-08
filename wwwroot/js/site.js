8dc8c06ce056ac433698b11652e849f5331fdd12
Halldór Valberg <halldorvs17@ru.is>
2018-05-08 21:10:32 +0000

    Nice Forsíða

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your JavaScript code.
+console.log("js ready");
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  12000);
